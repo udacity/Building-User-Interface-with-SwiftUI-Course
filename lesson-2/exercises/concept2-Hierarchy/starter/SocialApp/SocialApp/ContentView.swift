2bee53b657c3f153d1c2d6520c82f9d6675d383a
Mark DiFranco <mpdifranco@icloud.com>
2024-05-04 10:47:19 -0400

    Add starter code for Exercise: Hierarchy

---
+++ lesson-2/exercises/concept2-Hierarchy/starter/SocialApp/SocialApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialApp
+//
+//  Created by Mark DiFranco on 2024-05-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            // Add your social media post here
+        }
+        .listStyle(.plain)
+    }
+}
+
+#Preview {
+    ContentView()
+}
